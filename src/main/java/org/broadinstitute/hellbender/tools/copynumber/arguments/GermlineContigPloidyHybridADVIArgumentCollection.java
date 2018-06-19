cf615f929fa9ab4adee563bdc6eea039d2672e0b
Samuel Lee <slee@broadinstitute.org>
2018-06-19 16:36:02 -0400

    more, plot debugging

--- src/main/java/org/broadinstitute/hellbender/tools/copynumber/arguments/GermlineContigPloidyHybridADVIArgumentCollection.java
+++ src/main/java/org/broadinstitute/hellbender/tools/copynumber/arguments/GermlineContigPloidyHybridADVIArgumentCollection.java
@@ -17,7 +17,7 @@ public final class GermlineContigPloidyHybridADVIArgumentCollection extends Hybr
         defaultValue.put(HybridADVIArgument.LOG_EMISSION_SAMPLING_MEDIAN_REL_ERROR, 0.01);
         defaultValue.put(HybridADVIArgument.LOG_EMISSION_SAMPLING_ROUNDS, 100);
         defaultValue.put(HybridADVIArgument.MAX_ADVI_ITER_FIRST_EPOCH, 2000);
-        defaultValue.put(HybridADVIArgument.MAX_ADVI_ITER_SUBSEQUENT_EPOCHS, 500);
+        defaultValue.put(HybridADVIArgument.MAX_ADVI_ITER_SUBSEQUENT_EPOCHS, 1000);
         defaultValue.put(HybridADVIArgument.MIN_TRAINING_EPOCHS, 1);
         defaultValue.put(HybridADVIArgument.MAX_TRAINING_EPOCHS, 10);
         defaultValue.put(HybridADVIArgument.INITIAL_TEMPERATURE, 2.0);
